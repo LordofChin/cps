cb4e678335e1f1255b08952988dbf0a1a8d46247
tufts1cr <tufts1cr@cmich.edu>
2026-01-28 12:50:56 -0500

    fianl_ass2.asm

--- 360/asses/ass2/ass2.asm
+++ 360/asses/ass2/ass2.asm
@@ -3,8 +3,8 @@
 A: .word 67:201 	# array A of element size word (32-bit "int"), filled with 201 elements of 67. 
 e: .word 6		# e variable
 f: .word 7		# f variable
-.text
 
+.text
 # prestore locations of A[100] and A[200]
 la $s3, A		# load intial address of array into $s3 as specified
 li $t1, 100		# store int 100 			- for finding offset 
@@ -18,13 +18,13 @@ add $t2, $t1, $t2	# prestore loc(A[200]) in $t2 		- add prestored offest of 100 
 lw $t3, e		
 lw $t4, f		# load variables to add
 
-# compute 
-
-sub $t3, $t3, $t4	# find e - f segment
-lw $t4, 0($t1)		# load word/int at A[100] for + A[100] segment
-add $t3, $t3,  $t4	# find + A[100] segment
+# compute the equation
+sub $t3, $t3, $t4	# find "e - f" segment
+lw $t4, 0($t1)		# load word/int at A[100] for "+ A[100]" segment
+add $t3, $t3,  $t4	# calculate "+ A[100]" segment
 sw $t3, 0($t2)		# store results in prestored location of A[200]
 
+#print the results of the equation
 lw $a0, 0($t2)		# load result for verifying/printing by syscall's int-print service
 li $v0, 1		# load the int-print service for syscall to execute
 syscall			# execute the service stored in $v0 on word in $a0
